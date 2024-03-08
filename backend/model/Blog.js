c1e40a0d006abd8ef2ed5827453bc2cc4c824dc7
seyitbugraerden <seyitbugraerden@gmail.com>
2024-03-08 14:41:57 +0300

    Blog added username

--- backend/model/Blog.js
+++ backend/model/Blog.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const BlogSchema = mongoose.Schema(
   {
+    username: { type: String, required: true },
     title: { type: String, required: true },
     description: { type: String, required: true },
   },

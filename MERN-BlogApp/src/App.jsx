fc04707b2b5b01a995de9a4b6e3f95bd17c2f2d5
seyitbugraerden <seyitbugraerden@gmail.com>
2024-03-14 16:12:18 +0300

    Routes Fix

--- MERN-BlogApp/src/App.jsx
+++ MERN-BlogApp/src/App.jsx
@@ -6,14 +6,25 @@ import MainLayout from "./layout/MainLayout";
 import LoginPage from "./components/loginPage/loginPage";
 
 function App() {
-  const user = localStorage.getItem("user");
-
+  const user = JSON.parse(localStorage.getItem("user"));
   return (
     <Routes>
-      <Route path="/" element={user ? <MainLayout /> : <Navigate to="/login" />} />
-      <Route path="/detay/:id" element={user ? <BlogDetailLayout /> : <Navigate to="/login" />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
+      <Route
+        path="/"
+        element={user ? <MainLayout /> : <Navigate to="/login" />}
+      />
+      <Route
+        path="/detay/:id"
+        element={user ? <BlogDetailLayout /> : <Navigate to="/login" />}
+      />
+      <Route
+        path="/login"
+        element={!user ? <LoginPage /> : <Navigate to="/" />}
+      />
+      <Route
+        path="/register"
+        element={!user ? <RegisterPage /> : <Navigate to="/" />}
+      />
     </Routes>
   );
 }
